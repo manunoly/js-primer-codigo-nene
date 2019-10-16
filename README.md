712933b39413afe58209d4351cddd24202823d78
manunoly <manunoly@gmail.com>
2019-10-16 15:33:33 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# js-primer-codigo-nene
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/js-primer-codigo-nene)
---
+++ index.js
@@ -0,0 +1,23 @@
+// Import stylesheets
+import './style.css';
+
+// Write Javascript code!
+const appDiv = document.getElementById('app');
+appDiv.innerHTML = `<h1>JS Starter</h1>`;
+
+var nombre;
+var edad;
+var dia_cumple
+
+edad = 7;
+console.log(edad);
+// alert(edad);
+dia_cumple=27;
+
+if(dia_cumple == 27){
+  edad=8;
+
+}
+console.log(edad)
+
+
